f241bf365e97fd3b0356dc26b8b21f3d89c486e7
Sabu J <mail@jsabu.com>
2025-11-21 08:17:06 +0400

    restore index.html for Vite build, clean rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "./",
   plugins: [react()],
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      }
+    }
+  },
   preview: {
     host: "0.0.0.0",
     port: 5173,
